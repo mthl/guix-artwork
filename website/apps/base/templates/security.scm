1f7cd6c323c928d6e852acf5d8c746fa0fba010a
Leo Famulari <leo@famulari.name>
2021-04-05 16:04:43 -0400

    website: Alphabetically sort the list of security contacts.
    A small change, to avoid any suggestion of hierarchy or seniority.
    
    * website/apps/base/templates/security.scm (security-t): Sort by name.

--- website/apps/base/templates/security.scm
+++ website/apps/base/templates/security.scm
@@ -55,14 +55,14 @@ system|GNU Hurd|GNU Guix package manager|Security updates") #\|)
         (li "Ludovic Courtès"
           (ul (@ (class "mono"))
             (li ,ludovics-key)))
-        (li "Tobias Geerinckx-Rice"
-          (ul (@ (class "mono"))
-              (li (a (@ (href "https://www.tobias.gr/gpg/tobias.gr.gpg"))
-		     "F5BC 5534 C36F 0087 B39D  36EF 1C9D C4FE B9DB 7C4B"))))
         (li  "Maxime Devos"
           (ul (@ (class "mono"))
               (li (a (@ (href "https://notabug.org/mdevos/things/raw/master/Maxime_Devos.pub"))
-                     "C1F3 3EE2 0C52 8FDB 7DD7  011F 49E3 EE22 1917 25EE")))))
+                     "C1F3 3EE2 0C52 8FDB 7DD7  011F 49E3 EE22 1917 25EE"))))
+        (li "Tobias Geerinckx-Rice"
+          (ul (@ (class "mono"))
+              (li (a (@ (href "https://www.tobias.gr/gpg/tobias.gr.gpg"))
+		     "F5BC 5534 C36F 0087 B39D  36EF 1C9D C4FE B9DB 7C4B")))))
 
       ,(G_ `(h3 "Release signatures"))
       ,(G_

b45996c889dc7f40d3f3033b3960dde04e4e1047
Mathieu Othacehe <othacehe@gnu.org>
2021-04-10 20:48:00 +0200

    website: manifest: Fix indentation.
    * website/manifest.scm: Fix indentation.

--- website/manifest.scm
+++ website/manifest.scm
@@ -18,49 +18,49 @@
 
 (define locales
   (locale-directory
-    (call-with-input-file "po/LINGUAS"
-      (lambda (port)
-        (let loop ((line (read-line port))
-                   (locales '()))
-          (if (eof-object? line)
-              locales
-              (if (equal? (string-ref line 0) #\#)
-                  (loop (read-line port) locales)
-                  (loop (read-line port)
-                        (cons
-                          (locale-definition
-                            (name (string-append line ".utf8"))
-                            (source line))
-                          locales)))))))
-    #:libcs
-    (list glibc)))
+   (call-with-input-file "po/LINGUAS"
+     (lambda (port)
+       (let loop ((line (read-line port))
+                  (locales '()))
+         (if (eof-object? line)
+             locales
+             (if (equal? (string-ref line 0) #\#)
+                 (loop (read-line port) locales)
+                 (loop (read-line port)
+                       (cons
+                        (locale-definition
+                         (name (string-append line ".utf8"))
+                         (source line))
+                        locales)))))))
+   #:libcs
+   (list glibc)))
 
 (manifest
-  (cons
-    (manifest-entry
-      (name "locales")
-      (version "0")
-      (item (computed-file "locales"
-              (with-imported-modules '((guix build utils))
-                #~(let ((out (string-append #$output "/lib/locale")))
-                    (use-modules (guix build utils))
-                    (mkdir-p out)
-                    (copy-recursively #$locales out)))))
-      (search-paths
-        (list (search-path-specification
-                (variable "GUIX_LOCPATH")
-                (files '("lib/locale"))))))
-    (manifest-entries
-      (packages->manifest
-       (append
-        ;; Guile needs to be compatible
-        (list
-         guix
-         the-good-guile
-         haunt-the-ghost)
+ (cons
+  (manifest-entry
+    (name "locales")
+    (version "0")
+    (item (computed-file "locales"
+                         (with-imported-modules '((guix build utils))
+                           #~(let ((out (string-append #$output "/lib/locale")))
+                               (use-modules (guix build utils))
+                               (mkdir-p out)
+                               (copy-recursively #$locales out)))))
+    (search-paths
+     (list (search-path-specification
+            (variable "GUIX_LOCPATH")
+            (files '("lib/locale"))))))
+  (manifest-entries
+   (packages->manifest
+    (append
+     ;; Guile needs to be compatible
+     (list
+      guix
+      the-good-guile
+      haunt-the-ghost)
 
-        ;; Other packages
-        (map specification->package
-             (list
-              "git"
-              "guile-syntax-highlight")))))))
+     ;; Other packages
+     (map specification->package
+          (list
+           "git"
+           "guile-syntax-highlight")))))))
